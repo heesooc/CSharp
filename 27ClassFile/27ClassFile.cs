58acedd15e1de4f48e92465cb61ac428d27b6538
조희수 <hs.jo.sw.p@gmail.com>
2024-09-25 18:50:19 +0900

    partial

---
+++ 27ClassFile/27ClassFile.cs
@@ -0,0 +1,11 @@
+namespace _27ClassFile
+{
+    // ctrl + k + f 자동코드정렬
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer = new Player();
+        }
+    }
+}
---
+++ 28Partial/28Partial.cs
@@ -0,0 +1,13 @@
+namespace _28Partial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer = new Player();
+            NewPlayer.Func();
+            NewPlayer.Damage();
+        }
+    }
+}
+ 
